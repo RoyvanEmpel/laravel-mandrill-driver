d3900ae0d9aedeea47dc7751cbbb02cb3a8999b7
roy <roy@aniday.io>
2025-05-06 15:54:06 +0200

    Add mandrill send-template for transactional e-mails

--- src/MandrillTransport.php
+++ src/MandrillTransport.php
@@ -19,7 +19,10 @@ class MandrillTransport extends AbstractTransport
      * @param ApiClient $mailchimp
      * @param array $headers
      */
-    public function __construct(protected ApiClient $mailchimp, protected array $headers)
+    public function __construct(
+        protected ApiClient $mailchimp,
+        protected array $headers,
+        protected ?array $template = null)
     {
         parent::__construct();
     }
@@ -41,15 +44,13 @@ class MandrillTransport extends AbstractTransport
      */
     protected function doSend(SentMessage $message): void
     {
-        $data = $this->mailchimp->messages->sendRaw([
-            'raw_message' => $message->toString(),
-            'async' => true,
-            'to' => $this->getTo($message),
-        ]);
-
-        if ($data instanceof \GuzzleHttp\Exception\RequestException)
+        if (isset($this->template))
         {
-            throw new MandrillTransportException($data->getMessage(), $data->getCode(), $data);
+            $data = $this->sendTemplate($message);
+        }
+        else
+        {
+            $data = $this->sendRaw($message);
         }
 
         // If Mandrill _id was returned, set it as the message id for
@@ -63,6 +64,81 @@ class MandrillTransport extends AbstractTransport
     }
 
     /**
+     * Send the message using the Mandrill API /messages/send-raw endpoint.
+     * 
+     * @param SentMessage $message
+     * @throws \LaravelMandrill\MandrillTransportException
+     * @return mixed
+     */
+    private function sendRaw(SentMessage $message): mixed
+    {
+        $data = $this->mailchimp->messages->sendRaw([
+            'raw_message' => $message->toString(),
+            'async' => true,
+            'to' => $this->getTo($message),
+        ]);
+
+        if ($data instanceof \GuzzleHttp\Exception\RequestException)
+        {
+            throw new MandrillTransportException($data->getMessage(), $data->getCode(), $data);
+        }
+
+        return $data;
+    }
+
+    /**
+     * Send the message using the Mandrill API /messages/send-template endpoint.
+     * 
+     * @param \Symfony\Component\Mailer\SentMessage $message
+     * @throws \RuntimeException
+     * @throws \LaravelMandrill\MandrillTransportException
+     * @return mixed
+     */
+    private function sendTemplate(SentMessage $message): mixed
+    {
+        $originalMessage = $message->getOriginalMessage();
+
+        if (!$originalMessage instanceof Email)
+        {
+            throw new \RuntimeException('Original message is not an instance of Email');
+        }
+
+        $data = $this->mailchimp->messages->sendTemplate([
+            'template_name' => $this->template['template_name'],
+            'template_content' => [
+                (object)[
+                    'name' => $this->template['template_content'] ?? 'main',
+                    'content' => $originalMessage->getHtmlBody(),
+                ]
+            ],
+            'async' => true,
+            'message' => [
+                'from_email' => $originalMessage->getFrom()[0]->getAddress(),
+                'from_name' => $originalMessage->getReplyTo()[0]->getName(),
+                'subject' => $originalMessage->getSubject(),
+                'to' => $this->getToFull($message),
+                'attachments' => $this->getAttachments($originalMessage),
+
+                'headers' => [
+                    'Reply-To' => $originalMessage->getReplyTo()[0]->getAddress(),
+                ],
+
+                'global_merge_vars' => $this->getMergeVars($originalMessage, 'X-Global-Merge-Vars'),
+                'merge_vars' => $this->getMergeVars($originalMessage),
+
+                'tags' => $this->getTags($originalMessage),
+            ]
+        ]);
+
+        if ($data instanceof \GuzzleHttp\Exception\RequestException)
+        {
+            throw new MandrillTransportException($data->getMessage(), $data->getCode(), $data);
+        }
+
+        return $data;
+    }
+
+    /**
      * Retrieves recipients from the original message or envelope.
      *
      * @param SentMessage $message
@@ -72,24 +148,24 @@ class MandrillTransport extends AbstractTransport
     {
         $recipients = [];
 
-        $original_message = $message->getOriginalMessage();
+        $originalMessage = $message->getOriginalMessage();
 
-        if ($original_message instanceof Email) {
+        if ($originalMessage instanceof Email) {
 
-            if (!empty($original_message->getTo())) {
-                foreach ($original_message->getTo() as $to) {
+            if (!empty($originalMessage->getTo())) {
+                foreach ($originalMessage->getTo() as $to) {
                     $recipients[] = $to->getEncodedAddress();
                 }
             }
 
-            if (!empty($original_message->getCc())) {
-                foreach ($original_message->getCc() as $cc) {
+            if (!empty($originalMessage->getCc())) {
+                foreach ($originalMessage->getCc() as $cc) {
                     $recipients[] = $cc->getEncodedAddress();
                 }
             }
 
-            if (!empty($original_message->getBcc())) {
-                foreach ($original_message->getBcc() as $bcc) {
+            if (!empty($originalMessage->getBcc())) {
+                foreach ($originalMessage->getBcc() as $bcc) {
                     $recipients[] = $bcc->getEncodedAddress();
                 }
             }
@@ -103,6 +179,122 @@ class MandrillTransport extends AbstractTransport
         }
 
         return $recipients;
+    }
+
+    /**
+     * Retrieves recipients from the original message or envelope.
+     *
+     * @param SentMessage $message
+     * @return array
+     */
+    protected function getToFull(SentMessage $message): array
+    {
+        $recipients = [];
+
+        $originalMessage = $message->getOriginalMessage();
+
+        if ($originalMessage instanceof Email) {
+            if (!empty($originalMessage->getTo())) {
+                foreach ($originalMessage->getTo() as $to) {
+                    $recipients[] = [
+                        'email' => $to->getEncodedAddress(),
+                        'name' => $to->getName(),
+                        'type' => 'to',
+                    ];
+                }
+            }
+
+            if (!empty($originalMessage->getCc())) {
+                foreach ($originalMessage->getCc() as $cc) {
+                    $recipients[] = [
+                        'email'=> $cc->getEncodedAddress(),
+                        'name'=> $cc->getName(),
+                        'type'=> 'cc',
+                    ];
+                }
+            }
+
+            if (!empty($originalMessage->getBcc())) {
+                foreach ($originalMessage->getBcc() as $bcc) {
+                    $recipients[] = [
+                        'email'=> $bcc->getEncodedAddress(),
+                        'name'=> $bcc->getName(),
+                        'type'=> 'bcc',
+                    ];
+                }
+            }
+        }
+
+        // Fall-back to envelope recipients
+        if (empty($recipients)) {
+            foreach ($message->getEnvelope()->getRecipients() as $recipient) {
+                $recipients[] = [
+                    'email'=> $recipient->getEncodedAddress(),
+                    'name'=> $recipient->getName(),
+                    'type'=> 'to',
+                ];
+            }
+        }
+
+        return $recipients;
+    }
+
+    /**
+     * Retrieves attachments from the original message.
+     *
+     * @param SentMessage $message
+     * @return array
+     */
+    protected function getAttachments(Message $originalMessage): array
+    {
+        $attachments = [];
+
+        if ($originalMessage instanceof Email) {
+            foreach ($originalMessage->getAttachments() as $attachment) {
+                $attachments[] = [
+                    'type' => $attachment->getMediaType(),
+                    'name' => $attachment->getName(),
+                    'content' => base64_encode($attachment->getBody()),
+                ];
+            }
+        }
+
+        return $attachments;
+    }
+
+    protected function getMergeVars(Message $originalMessage, string $varsHeader = 'X-Merge-Vars'): array
+    {
+        $headers = $originalMessage->getHeaders();
+        $varsHeader = $headers->get($varsHeader);
+
+        $vars = [];
+
+        if ($varsHeader) {
+            $decoded = json_decode($varsHeader->getBody(), true);
+            if (is_array($decoded)) {
+                foreach ($decoded as $name => $content) {
+                    $vars[] = [
+                        'name' => $name,
+                        'content' => $content,
+                    ];
+                }
+            }
+        }
+
+        return $vars;
+    }
+
+    protected function getTags(Message $originalMessage): array
+    {
+        $tags = [];
+
+        if ($originalMessage instanceof Email && $originalMessage->getHeaders()->has('X-Tag')) {
+            foreach ($originalMessage->getHeaders()->all('X-Tag') as $header) {
+                $tags[] = $header->getBodyAsString();
+            }
+        }
+
+        return $tags;
     }
 
     /**
